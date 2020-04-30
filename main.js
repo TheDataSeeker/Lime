cfbc8a57fecce78e3bc749aa53fe630e36742eb4
TheDataSeeker <51262107+TheDataSeeker@users.noreply.github.com>
2020-05-01 01:25:49 +1000

    Size and basic thingy

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 800,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
